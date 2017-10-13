93b9fc0ca8175cd2531611cde2f432b8c2344554
bmerkle3 <braidymerkle@gmail.com>
2017-10-12 22:30:46 -0500

    add devise for users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def new
@@ -9,12 +10,16 @@ class PostsController < ApplicationController
 
   def create
   @post = Post.create( post_params )
+  if @post.save
+    redirect_to posts_path
+  else
+    redirect_to new_post_path
+  end
   end
 
 private
-
-
 def post_params
-  params.require(:post).permit(:image)
+  params.require(:post).permit(:image, :caption)
 end
+
 end
